7533e13e4053fd866f9e4ab734714900b88a4367
otarza <otar.zakalashvili@gmail.com>
2024-11-12 11:31:17 +0400

    Add pages for JS section.

---
+++ docs/out/_next/static/chunks/nextra-page-map-.mjs
@@ -0,0 +1,54 @@
+import meta from "../../../pages/_meta.js";
+import javascript_meta from "../../../pages/javascript/_meta.js";
+import sql_meta from "../../../pages/sql/_meta.js";
+export const pageMap = [{
+  data: meta
+}, {
+  name: "index",
+  route: "/",
+  frontMatter: {
+    "sidebarTitle": "Index"
+  }
+}, {
+  name: "javascript",
+  route: "/javascript",
+  children: [{
+    data: javascript_meta
+  }, {
+    name: "index",
+    route: "/javascript",
+    frontMatter: {
+      "sidebarTitle": "Index"
+    }
+  }]
+}, {
+  name: "sql",
+  route: "/sql",
+  children: [{
+    data: sql_meta
+  }, {
+    name: "ch0",
+    route: "/sql/ch0",
+    frontMatter: {
+      "sidebarTitle": "Ch0"
+    }
+  }, {
+    name: "ch1",
+    route: "/sql/ch1",
+    frontMatter: {
+      "sidebarTitle": "Ch1"
+    }
+  }, {
+    name: "ch2",
+    route: "/sql/ch2",
+    frontMatter: {
+      "sidebarTitle": "Ch2"
+    }
+  }, {
+    name: "index",
+    route: "/sql",
+    frontMatter: {
+      "sidebarTitle": "Index"
+    }
+  }]
+}];
---
+++ docs/out/_next/static/chunks/pages/javascript/_meta-902d48e1d861aa75.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[43],{6055:(_,e,u)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/javascript/_meta",function(){return u(5735)}])},5735:(_,e,u)=>{"use strict";u.r(e),u.d(e,{__N_SSG:()=>a,default:()=>n});var a=!0;let n=()=>null}},_=>{var e=e=>_(_.s=e);_.O(0,[888,774,179],()=>e(6055)),_N_E=_.O()}]);
