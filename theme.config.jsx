f4af5ba0f07665317e6be0c5eba97e7f3a79e2d4
otarza <otar.zakalashvili@gmail.com>
2024-11-12 11:49:00 +0400

    Cleanup empty pages.

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,7 +1,7 @@
 export default {
     logo: <span>BitCamp 🔥✊</span>,
     project: {
-      link: 'https://github.com/shuding/nextra'
+      link: 'https://github.com/otarza/programireba.ge'
     }
     // ... other theme options
   }
