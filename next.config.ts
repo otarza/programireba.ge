d9cd458d43fa55745174b5a6c606625f9b28e8a7
otarza <otar.zakalashvili@gmail.com>
2025-02-13 21:39:54 +0400

    Adding chapter 2 and 3 for HTML.

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,7 @@ import nextra from "nextra";
 
 
 const withNextra = nextra({
-  theme: 'nextra-theme-docs',
-  'themeConfig': './theme.config.jsx'
+  theme: 'nextra-theme-docs'
 })
 
 export default withNextra({
