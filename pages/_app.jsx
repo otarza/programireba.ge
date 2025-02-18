1b03c2dc31d3f9cb386ee6f2b1fe8e8cb2143434
otarza <otar.zakalashvili@gmail.com>
2025-02-18 18:35:05 +0400

    Add Google Analytics and Meta Pixel scripts.

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,3 +1,11 @@
+import Script from "next/script";
+
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return (
+      <>
+        <Component {...pageProps} />
+        <Script src="/scripts/ga.js" />
+        <Script src="/scripts/pixel.js" />
+      </>
+    );
   }
