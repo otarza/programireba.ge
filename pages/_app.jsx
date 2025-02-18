9109230ade1725b84d01f472948d249fc762780b
otarza <otar.zakalashvili@gmail.com>
2025-02-18 18:38:15 +0400

    Add Gtag script.

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -4,6 +4,7 @@ export default function App({ Component, pageProps }) {
     return (
       <>
         <Component {...pageProps} />
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-N2VFCQNXQ4" />
         <Script src="/scripts/ga.js" />
         <Script src="/scripts/pixel.js" />
       </>
